1f900af94bdc49a051168e61b7d928bd4a3f1c7d
Kelly Mears <developers@tinypixel.dev>
2020-06-10 14:59:55 -0700

    [wip] bud docs

--- docs/bud/master.js
+++ docs/bud/master.js
@@ -1,9 +1,26 @@
 module.exports = [
   {
-    title: 'Getting Started',
+    title: 'Installation',
     collapsable: false,
     children: [
       'master/installation',
+      'master/using-the-init-command',
+      'master/using-a-preset',
+      'master/installing-manually',
+    ],
+  },
+  {
+    title: 'Getting started',
+    collapsable: false,
+    children: [
+      'master/project-structure',
+    ],
+  },
+  {
+    title: 'Extending Bud',
+    collapsable: false,
+    children: [
+      'master/extending-bud',
     ],
   },
 ];
--- docs/bud/master/installation.md
+++ docs/bud/master/installation.md
@@ -1,1 +1,11 @@
+---
+description: Installing Bud requires node 12 and yarn.
+---
+
 # Installation
+
+Installing Bud globally is not recommended or supported.
+
+::: tip Bud requires the use of yarn
+While there are plans to support `npm` in the near future, Bud features which modify node_modules currently require `yarn`.
+:::
