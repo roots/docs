eaf757280a14e32dccff181d8cb9c6f7d2dbb97f
Ben Word <ben@benword.com>
2021-11-03 17:13:21 -0500

    Acorn docs starting point

---
+++ docs/acorn/2.x.js
@@ -0,0 +1,21 @@
+module.exports = [
+  {
+    title: 'Getting Started',
+    collapsable: false,
+    children: [
+      '2.x/installation',
+    ],
+  },
+  {
+    title: 'The Basics',
+    children: [
+      '2.x/tbd',
+    ],
+  },
+  {
+    title: 'Digging Deeper',
+    children: [
+      '2.x/tbd',
+    ],
+  },
+]
