ee3e4d235d83c8ffc82b69967a7aa5b63ccdfb16
Ben Word <ben@benword.com>
2021-11-26 18:17:50 -0600

    update redirects

--- docs/.vuepress/enhanceApp.js
+++ docs/.vuepress/enhanceApp.js
@@ -1,7 +1,15 @@
 import '@theme/styles/index.scss';
 
-export default ({
-  Vue
-}) => {
-  //
+export default ({ router }) => {
+  router.addRoutes([
+    { path: '/acorn/', redirect: '/acorn/2.x/installation/' },
+    { path: '/bedrock/', redirect: '/bedrock/master/installation/' },
+    { path: '/examples/', redirect: '/examples/roots-example-project/' },
+    { path: '/getting-started/', redirect: '/getting-started/macos/' },
+    { path: '/sage/', redirect: '/sage/10.x/installation/' },
+    { path: '/sage/8.x', redirect: '/sage/8.x/installation/' },
+    { path: '/sage/9.x', redirect: '/sage/9.x/installation/' },
+    { path: '/trellis/', redirect: '/trellis/master/installation/' },
+    { path: '/trellis-cli/', redirect: '/trellis-cli/master/installation/' },
+  ])
 }
